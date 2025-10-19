a981b4655389e03e2a14558b7f8e50561a214f4a
mahmud-21 <mmr106406@gmail.com>
2025-10-19 23:05:05 +0600

    Implement RectangleArea class to calculate area

--- Assingment-3/RectangleArea.java
+++ Assingment-3/RectangleArea.java
@@ -1,1 +1,18 @@
+class RectangleArea {
+    public static void main(String[] args) {
+               if (args.length < 2) {
+            System.out.println("Please provide both width and height as command line arguments.");
+            return;
+        }
+
+               double width = Double.parseDouble(args[0]);
+        double height = Double.parseDouble(args[1]);
+
+               double area = width * height;
+
+               System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
 
