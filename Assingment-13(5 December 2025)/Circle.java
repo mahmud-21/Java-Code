2ff981b92965bd0ad1b478dc90fbb96e3709c3e0
mahmud-21 <mmr106406@gmail.com>
2026-01-21 11:39:55 +0600

    Update Circle.java

--- Assingment-13(5 December 2025)/Circle.java
+++ Assingment-13(5 December 2025)/Circle.java
@@ -1,1 +1,21 @@
-
+public class Circle {
+    private double radius;
+    
+    public Circle() {}
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    public void setRadius(double rad) {
+        this.radius = rad;
+    }
+    
+    public double getRadius() {
+        return this.radius;
+    }
+    
+    public double computeArea() {
+        return (radius * radius * Math.PI);
+    }
+}
