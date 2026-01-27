00877ded17c703fe53983d8cc04c9d428cda3700
mahmud-21 <mmr106406@gmail.com>
2026-01-28 00:01:35 +0600

    Change input method for diagonals in RhombusArea
    Updated the program to read diagonals from user input instead of command line arguments.

--- Assingment-3/RhombusArea.java
+++ Assingment-3/RhombusArea.java
@@ -1,15 +1,14 @@
+import java.util.Scanner;
 
-class RhombusArea {
+public class RhombusArea {
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Please provide both diagonals (d1 and d2) as command line arguments.");
-            return;
-        }
-        double d1 = Double.parseDouble(args[0]);
-        double d2 = Double.parseDouble(args[1]);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first diagonal (d1): ");
+        double d1 = sc.nextDouble();
+        System.out.print("Enter second diagonal (d2): ");
+        double d2 = sc.nextDouble();
         double area = (d1 * d2) / 2;
-        System.out.println("Diagonal 1: " + d1);
-        System.out.println("Diagonal 2: " + d2);
-        System.out.println("Area of Rhombus: " + area);
+        System.out.println("Area of Rhombus = " + area);
+        sc.close();
     }
 }
