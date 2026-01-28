a07257f06b4706a947e0088abb019caa214a3b3e
mahmud-21 <mmr106406@gmail.com>
2026-01-28 15:32:38 +0600

    Update Main.java

--- Lab Final and Quiz - ICT 21th batch (OOP 1.5 lab)/Question 3: Abstract Class vs Interface/Main.java
+++ Lab Final and Quiz - ICT 21th batch (OOP 1.5 lab)/Question 3: Abstract Class vs Interface/Main.java
@@ -1,1 +1,54 @@
+interface Drawable {
+    void draw();
+}
+abstract class Shape {
+    abstract double area();
+}
+class Circle extends Shape implements Drawable {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return 3.14 * radius * radius;
+    }
+
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+class Rectangle extends Shape implements Drawable {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Shape c = new Circle(5);
+        Shape r = new Rectangle(4, 6);
+
+        System.out.println("Circle Area: " + c.area());
+        System.out.println("Rectangle Area: " + r.area());
+
+        Drawable d1 = new Circle(3);
+        Drawable d2 = new Rectangle(2, 5);
+
+        d1.draw();
+        d2.draw();
+    }
+}
+
 
