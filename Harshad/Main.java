8981dc38e359f7bdf357c38ecee59b0f4436d2b9
mahmud-21 <mmr106406@gmail.com>
2026-05-06 10:12:46 +0600

    Implement Harshad number check in Main.java

---
+++ Harshad/Main.java
@@ -0,0 +1,31 @@
+class Harshad {
+    int number;
+
+    Harshad(int n) {
+        number = n;
+    }
+
+    public String displayInfo() {
+        int temp = number;
+        int sum = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit;
+            temp = temp / 10;
+        }
+
+        if (number % sum == 0) {
+            return "Harshad Number. Sum = " + sum;
+        } else {
+            return "Not a Harshad Number";
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Harshad n1 = new Harshad(89);
+        System.out.println(n1.displayInfo());
+    }
+}
